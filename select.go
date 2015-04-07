977274d4fd91f50aa2bd0ad4342b8e4006d82d2d
Marc Grol <mgrol@xebia.com>
2015-04-07 13:13:14 +0200

    adjust select

--- select.go
+++ select.go
@@ -6,19 +6,25 @@ import (
 )
 
 // START OMIT
+func sendMsg(c chan string) { // HL
+	time.Sleep(50 * time.Millisecond) // HL
+	c <- "hi there"                   // HL
+} // HL
+
 func main() {
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(300 * time.Millisecond)
+	msgChannel := make(chan string) // HL
+	go sendMsg(msgChannel)          // HL
 	for {
 		select {
+		case msg := <-msgChannel:
+			fmt.Printf("msg: %s\n", msg)
 		case <-tick:
 			fmt.Println("tick.")
 		case <-boom:
 			fmt.Println("BOOM!")
 			return
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
