8394d05923524770bc7b336fe0332e09d7fe63e7
Marc Grol <mgrol@xebia.com>
2017-09-27 23:23:08 +0200

    minor fixes

--- functions/functions.go
+++ functions/functions.go
@@ -3,7 +3,7 @@ package functions
 import "fmt"
 
 // START OMIT
-func ConvertIt(arg int) string { // HL
+func ConvertIt(arg int) string { // exported function // HL
 	return convertInternal(arg)
 }
 func convertInternal(arg int) string { // internal, unexported function
