355e416f247792ddb91ff7e866bfe22ab148b46f
Albert Brand <abrand@xebia.com>
2018-01-22 16:06:09 +0100

    Add inline template example to slides

--- template/template.go
+++ template/template.go
@@ -21,27 +21,27 @@ func (c *Child) NameAndAge() string {
 	return fmt.Sprintf("%s (%d)", c.Name, c.Age)
 }
 
+var p = Person{
+	Name:      "Marc Grol",
+	Interests: []string{"Running", "Golang"},
+	Children: []Child{
+		{Name: "Pien", Age: 5},
+		{Name: "Tijl", Age: 12},
+		{Name: "Freek", Age: 18},
+	},
+}
+
 func main() {
 	const tpl = `
 <html>
 	<body>
 		<p>Hi, I'm {{.Name}}.
-		<p>I like {{range .Interests}}{{.}},{{end}}
+		<p>I like {{range .Interests}}{{.}}, {{end}}
 		<ul>
 		{{range .Children}}<li>{{.NameAndAge}}</li>{{end}}
 		</ul>
 	</body>
 </html>`
-
-	p := Person{
-		Name:      "Marc Grol",
-		Interests: []string{"Running", "Golang"},
-		Children: []Child{
-			{Name: "Pien", Age: 5},
-			{Name: "Tijl", Age: 12},
-			{Name: "Freek", Age: 18},
-		},
-	}
 
 	t, _ := template.New("person").Parse(tpl)
 	_ = t.Execute(os.Stdout, p)
