ae776fe474a5ecf483b23b4906173be072b97e51
Albert Brand <abrand@xebia.com>
2018-01-22 21:33:00 +0100

    Update slides

--- if/if.go
+++ if/if.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// START OMIT
 func main() {
 	num := 9
 	if num < 0 { // HL
@@ -13,4 +14,15 @@ func main() {
 	} else { // HL
 		fmt.Println(num, "has multiple digits")
 	}
+
+	if smallerNum := minus10(num); smallerNum > 0 {  // HL
+		fmt.Println(smallerNum, " is larger than 0")
+	} else {
+		fmt.Println(smallerNum, " is smaller than 0")
+	}
 }
+
+func minus10(a int) int {
+	return a - 10
+}
+// END OMIT
