1aeb076b5ca610d7ae6fe1b091076971e377a3ee
marcgrol <mgrol@xebia.com>
2019-12-03 20:27:43 +0100

    Add swagger spec

--- examples/project/notificationService/spec/spec.go
+++ examples/project/notificationService/spec/spec.go
@@ -1,6 +1,6 @@
 package notification
 
-//go:generate protoc -I/usr/local/include -I ../spec --go_out=plugins=grpc:../spec --grpc-gateway_out=logtostderr=true:../spec ../spec/notification.proto
+//go:generate protoc -I/usr/local/include -I ../spec --go_out=plugins=grpc:../spec --grpc-gateway_out=logtostderr=true:../spec --swagger_out=logtostderr=true:../specg ../spec/notification.proto
 
 func init() {
 
