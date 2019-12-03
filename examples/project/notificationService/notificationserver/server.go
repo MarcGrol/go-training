a206eb9b9ee89c07bea8f99e66fa3c43008f296d
marcgrol <mgrol@xebia.com>
2019-12-03 20:25:00 +0100

    Add comments

--- examples/project/notificationService/notificationserver/server.go
+++ examples/project/notificationService/notificationserver/server.go
@@ -47,26 +47,29 @@ func (s *server)GRPCListenBlocking(port string) error {
 }
 
 func (s *server)ListenHttpBlocking(grpcPort string, restPort string) error{
+	// start GRPC server in the background
 	go func() {
 		err := s.GRPCListenBlocking(grpcPort)
 		if err != nil {
 			log.Fatalf("Error starting grpc-notification server: %s", err)
 		}
 	}()
+
+	// give it some time to startup
 	time.Sleep(2* time.Second)
 
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	// Connect to the GRPC server
+	// Connect to the just created GRPC server
 	conn, err := grpc.Dial(grpcPort, grpc.WithInsecure())
 	if err != nil {
 		return fmt.Errorf("fail to dial: %v", err)
 	}
 	defer conn.Close()
 
-	// Register grpc-gateway
+	// Register REST gateway that proxies rest to grpc and back
 	rmux := runtime.NewServeMux()
 	client := pb.NewNotificationClient(conn)
 	err = pb.RegisterNotificationHandlerClient(ctx, rmux, client)
@@ -76,12 +79,13 @@ func (s *server)ListenHttpBlocking(grpcPort string, restPort string) error{
 	log.Println("REST server starts listening...")
 	err = http.ListenAndServe(restPort, rmux)
 	if err != nil {
-		return fmt.Errorf("Error listening on http:%s", err)
+		return fmt.Errorf("Error listening on http: %s", err)
 	}
 	return nil
 }
 
 func (s *server) SendEmail(ctx context.Context, in *pb.SendEmailRequest) (*pb.SendEmailReply, error) {
+	// TODO check mandatory parameters
 	log.Printf("Send email to '%s' with subject '%s'", in.GetEmail().GetRecipientEmailAddress(), in.GetEmail().GetSubject())
 	if strings.Contains(in.GetEmail().GetSubject(), "5") {
 		return &pb.SendEmailReply{
@@ -99,6 +103,7 @@ func (s *server) SendEmail(ctx context.Context, in *pb.SendEmailRequest) (*pb.Se
 }
 
 func (s *server) SendSms(ctx context.Context, in *pb.SendSmsRequest) (*pb.SendSmsReply, error) {
+	// TODO check mandatory parameters
 	log.Printf("Send sms to '%s' with body '%s'", in.GetSms().GetRecipientPhoneNumber(), in.GetSms().GetBody())
 	msgUID := uuid.New().String()
 	status := pb.NotificationStatus_PENDING
@@ -107,10 +112,12 @@ func (s *server) SendSms(ctx context.Context, in *pb.SendSmsRequest) (*pb.SendSm
 }
 
 func (s *server) GetNotificationStatus(ctx context.Context, in *pb.GetNotificationStatusRequest) (*pb.GetNotificationStatusReply, error) {
+	// TODO check mandatory parameters
 	log.Printf("Get status of notification with uid '%s'", in.GetNotificationUid())
 	status, found := s.messages[in.GetNotificationUid()]
 	if !found {
 		return &pb.GetNotificationStatusReply{
+			Status:pb.NotificationStatus_UNKNOWN,
 			Error: &pb.Error{
 				Code:                 404,
 				Message:              "Notification with uid not found",
