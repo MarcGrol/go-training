3483f53c05ead6ac466706134c8a2fd22b68f08d
marcgrol <mgrol@xebia.com>
2019-12-10 23:09:02 +0100

    automate curl commands

--- examples/webserver/testit.sh
+++ examples/webserver/testit.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 
-export PATIENT_UID="2ef04552-8983-48b2-b267-15aa48243d23"
+# create
+export PATIENT_UID=$(curl -s \
+    -X POST \
+    -H 'Accept: application/json' -H 'Content-Type: application/json' \
+     --data '{"FullName":"Marc Grol","AddressLine":"Heemstrakwartier 79","Allergies":["gezeik"]}' \
+     https://gotrainingxebia.appspot.com/api/patient | jq --raw-output '.UID')
 
-curl -vvv \
+# get
+curl -s \
     -X GET \
     -H 'Accept: application/json' \
     https://gotrainingxebia.appspot.com/api/patient/${PATIENT_UID}
 
-curl -vvv \
-    -X POST \
-    -H 'Accept: application/json' -H 'Content-Type: application/json' \
-     --data '{"FullName":"Marc Grol","AddressLine":"Heemstrakwartier 79","Allergies":["gezeik"]}' \
-     https://gotrainingxebia.appspot.com/api/patient
-
-curl -vvv \
+# modify
+curl -s \
     -X PUT \
     -H 'Accept: application/json' -H 'Content-Type: application/json' \
      --data '{"FullName":"Marc Grol","AddressLine":"Thuis","Allergies":["gedoe"]}' \
      https://gotrainingxebia.appspot.com/api/patient/${PATIENT_UID}
 
-curl -vvv \
+# get
+curl -s \
+    -X GET \
+    -H 'Accept: application/json' \
+    https://gotrainingxebia.appspot.com/api/patient/${PATIENT_UID}
+
+# delete
+curl -s \
     -X DELETE \
      https://gotrainingxebia.appspot.com/api/patient/${PATIENT_UID}
+
+# get
+curl -s \
+    -X GET \
+    -H 'Accept: application/json' \
+    https://gotrainingxebia.appspot.com/api/patient/${PATIENT_UID}
