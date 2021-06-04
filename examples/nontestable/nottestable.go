7623f60a47ade6b81ac3982fb0dd1865ea9e9cc3
marcgrol <mgrol@xebia.com>
2021-06-04 13:06:40 +0200

    small refinements

--- examples/nontestable/nottestable.go
+++ examples/nontestable/nottestable.go
@@ -2,13 +2,14 @@ package nontestable
 
 import (
 	"io/ioutil"
+	"strings"
 	"time"
 
 	"github.com/google/uuid" // third-party package
 )
 
 func Write() error {
-	u := uuid.New()
-	ft := time.Now().Format(time.RFC3339)
-	return ioutil.WriteFile(u.String()+".txt", []byte(ft), 0644)
+	filename := strings.ToUpper(uuid.New().String())
+	future := time.Now().AddDate(1, 2, 3).Format(time.RFC3339)
+	return ioutil.WriteFile(filename+".txt", []byte(future), 0644)
 }
