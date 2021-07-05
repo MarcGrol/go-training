3a8a87374204210cee24c90a64e56c0929938cde
marcgrol <mgrol@xebia.com>
2021-07-05 16:12:52 +0200

    Add anonymous structs

---
+++ examples/anonymous/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "log"
+
+// START OMIT
+func main() {
+	persons := []struct { // HL
+		name string // HL
+		age  int    // HL
+	}{ // HL
+		{name: "Marc", age: 50},  // HL
+		{name: "Eva", age: 48},  // HL
+		{name: "Pien", age: 18},  // HL
+		{name: "Tijl", age: 15},  // HL
+		{name: "Freek", age: 11},  // HL
+	} // HL
+
+	for idx, p := range persons {
+		log.Printf("person %d: %+v", idx, p)
+	}
+}
+// END OMIT
---
+++ examples/sorting/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"sort"
+)
+
+// START OMIT
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	persons := []Person{
+		{name: "Marc", age: 50},
+		{name: "Eva", age: 48},
+		{name: "Pien", age: 18},
+		{name: "Tijl", age: 15},
+		{name: "Freek", age: 11},
+	}
+	// END OMIT
+	// sort in age "ascending"
+	sort.Slice(persons, func(i, j int) bool {
+		return persons[i].age < persons[j].age
+	})
+	log.Printf("%+v", persons)
+}
