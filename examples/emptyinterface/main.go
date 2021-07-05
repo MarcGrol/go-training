928c55d0300623ccf4020ec77ae7fe3976c51e44
marcgrol <mgrol@xebia.com>
2021-07-05 23:23:13 +0200

    little change

--- examples/emptyinterface/main.go
+++ examples/emptyinterface/main.go
@@ -2,17 +2,17 @@ package main
 
 import "fmt"
 
+// START OMIT
 func main() {
 	var anything interface{} = 1
 	fmt.Printf("anything int: %+v\n", anything)
 
 	anything = "marc"
-	fmt.Printf("anything string: %+v\n", anything)
-
-	anything = false
 	print(anything)
 }
 
 func print(it interface{}) {
 	fmt.Printf("anything: %+v\n", it)
 }
+
+// END OMIT
