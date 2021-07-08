0ff1f26ab39293ac94a30bab34db963d47d46ad7
marcgrol <mgrol@xebia.com>
2021-07-08 13:40:47 +0200

    Dedicated json and xml serializers

--- examples/encoding/mydate.go
+++ examples/encoding/mydate.go
@@ -12,23 +12,21 @@ type MyDate struct {
 }
 
 func (t MyDate) MarshalJSON() ([]byte, error) {
-	bytes, err := json.Marshal(t.UnderlyingTime.Format("02-01-2006"))
-	if err != nil {
-		return nil, fmt.Errorf("Error encoding my-date: %+v: %s", t, err)
-	}
-	return bytes, nil
+	formattedDate := t.UnderlyingTime.Format("02-01-2006")
+	return json.Marshal(formattedDate)
 }
 
 func (t *MyDate) UnmarshalJSON(data []byte) error {
 	var dateAsString string
 	err := json.Unmarshal(data, &dateAsString)
 	if err != nil {
-		return fmt.Errorf("Error decoding my-date: %+v: %s", data, err)
+		return fmt.Errorf("Error decoding json my-date as string: %+v: %s", data, err)
 	}
+
 	dateAsString = strings.Trim(dateAsString, `"`)
 	date, err := time.Parse("02-01-2006", dateAsString)
 	if err != nil {
-		return fmt.Errorf("Error parsing my-date: %s: %s", dateAsString, err)
+		return fmt.Errorf("Error parsing json my-date: %s: %s", dateAsString, err)
 	}
 	*t = MyDate{
 		UnderlyingTime: date,
@@ -37,17 +35,15 @@ func (t *MyDate) UnmarshalJSON(data []byte) error {
 }
 
 func (t MyDate) MarshalText() ([]byte, error) {
-	formatted := t.UnderlyingTime.Format("02-01-2006")
-	return []byte(formatted), nil
+	formattedDate := t.UnderlyingTime.Format("02-01-2006")
+	return []byte(formattedDate), nil
 }
 
 func (t *MyDate) UnmarshalText(data []byte) error {
-	var dateAsString string
-
-	dateAsString = strings.Trim(dateAsString, `"`)
+	var dateAsString string = string(data)
 	date, err := time.Parse("02-01-2006", dateAsString)
 	if err != nil {
-		return fmt.Errorf("Error parsing my-date: %s: %s", dateAsString, err)
+		return fmt.Errorf("Error parsing xml-my-date: %s: %s", dateAsString, err)
 	}
 	*t = MyDate{
 		UnderlyingTime: date,
