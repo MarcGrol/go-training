5da8e22dc85d9349a1b85955a50eeafdcb18bcac
marcgrol <mgrol@xebia.com>
2021-07-08 10:18:57 +0200

    fixes

--- examples/channels/channels.go
+++ examples/channels/channels.go
@@ -14,15 +14,15 @@ func sum(a []int, resultChannel chan<- int) { // chan<-: Only allows writing to
 
 func doit() {
 	// Create result channel // HL
-	responseChannel := make(chan int) // construct channel
-	defer close(responseChannel)      // prevent resource leak
+	responseChannel := make(chan int) // construct blocking channel
+	defer close(responseChannel)      // knows when all writers are done, so can close
 
 	// Divide the work over multiple go-routines that run in background // HL
 	go sum([]int{1, 2, 3}, responseChannel) // 1 + 2 + 3 = 6
 	go sum([]int{4, 5, 6}, responseChannel) // 4 + 5 + 6 = 15
 
 	// Waitfor all background tasks to have completed 	// HL
-	x, y := <-responseChannel, <-responseChannel // receive from channels
+	x, y := <-responseChannel, <-responseChannel // receive twice from channels
 
 	// Continue with result // HL
 	fmt.Printf("sum=%d", x+y) // order undefined
--- examples/slowapi/summer.go
+++ examples/slowapi/summer.go
@@ -9,8 +9,13 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+// Sum can be used to simulate a slow action
 func Sum(a, b int) int {
+	// Sleep-time:
+	// - min: 500 msec
+	// - average 1000 msec
+	// - max: 1500 msec
 	sleepDurationInMillisec := 500 + (rand.Intn(1000))
-	time.Sleep(time.Duration(sleepDurationInMillisec) * time.Millisecond) // sleep approx. 1 sec
-	return a * b
+	time.Sleep(time.Duration(sleepDurationInMillisec) * time.Millisecond)
+	return a + b
 }
