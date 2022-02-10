f9cd8962492647e12957b1a5679e4fce6fb54b1e
Patrick Akil <patrickakil@hotmail.com>
2022-02-10 21:23:24 +0100

    reuse function variables instead of global ones

--- solutions/errorhandling/main.go
+++ solutions/errorhandling/main.go
@@ -7,12 +7,9 @@ import (
 	"strings"
 )
 
-const (
-	filename       = "./solutions/errorhandling/main.go"
-	filenameInCaps = filename + ".txt"
-)
-
 func main() {
+	const filename = "./solutions/errorhandling/main.go"
+	const filenameInCaps = filename + ".txt"
 	err := capitalizeFileContent(filename, filenameInCaps)
 	if err != nil {
 		log.Fatalf("Error reading file %s: %s", filename, err)
@@ -20,18 +17,17 @@ func main() {
 }
 
 func capitalizeFileContent(inFilename, outFilename string) error {
-	data, err := os.ReadFile(filename)
+	data, err := os.ReadFile(inFilename)
 	if err != nil {
-		return fmt.Errorf("Error reading file %s: %s", filename, err)
+		return fmt.Errorf("error reading file %s: %s", inFilename, err)
 	}
 
 	inCaps := strings.ToUpper(string(data))
-	//inCaps := bytes.ToUpper(data)
 	log.Printf("In caps: %s", inCaps)
 
-	err = os.WriteFile(filenameInCaps, []byte(inCaps), 0644)
+	err = os.WriteFile(outFilename, []byte(inCaps), 0644)
 	if err != nil {
-		return fmt.Errorf("Error writing file %s: %s", filenameInCaps, err)
+		return fmt.Errorf("error writing file %s: %s", outFilename, err)
 	}
 
 	return nil
