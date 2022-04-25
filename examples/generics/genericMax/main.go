e191edfdf0cc29c0bb0e2757cf125eb260d76690
Patrick Akil <patrickakil@hotmail.com>
2022-04-25 21:12:57 +0200

    Expand generic max examples

--- examples/generics/genericMax/main.go
+++ examples/generics/genericMax/main.go
@@ -8,16 +8,20 @@ import (
 // START OMIT
 
 func main() {
-	fmt.Println(MaxFloat64(5.1, 1.3))
-	fmt.Println(MaxInt(101, 23))
-	fmt.Println(MaxString("apple", "banana"))
+	fmt.Println(MaxFloat32(5.311, 5.323))
+	fmt.Println(MaxFloat64(5.302941, 5.3209943))
+	fmt.Println(MaxInt(-101, 23))
+	fmt.Println(MaxInt32(10231, 23234))
+	fmt.Println(MaxInt64(10234231, 124))
+	fmt.Println(MaxUint(101, 23))
 
 	fmt.Println(Max(5.1, 1.3))
 }
 
 // Max takes custom type parameter T of type constraints.Ordered: any type that is order-able
-// Ordered is a constraint that permits any ordered type: any type
-// that supports the operators < <= >= >.
+// Ordered is a constraint that permits any ordered type:
+// any type that supports the operators < <= >= >.
+// constraints.Ordered is still in experimental phase
 func Max[T constraints.Ordered](x T, y T) T {
 	if x > y {
 		return x
@@ -34,6 +38,13 @@ func MaxFloat64(x float64, y float64) float64 {
 	return y
 }
 
+func MaxFloat32(x float32, y float32) float32 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func MaxInt(x int, y int) int {
 	if x > y {
 		return x
@@ -41,7 +52,21 @@ func MaxInt(x int, y int) int {
 	return y
 }
 
-func MaxString(x string, y string) string {
+func MaxInt32(x int32, y int32) int32 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MaxInt64(x int64, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func MaxUint(x uint, y uint) uint {
 	if x > y {
 		return x
 	}
